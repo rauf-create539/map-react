4dbe7127ac3b8bedd3cfc07d1817f7db829b6f8f
rauf-create539 <raufimar44@gmail.com>
2025-10-03 18:14:58 +0800

    With tailwindcss

--- map-create/src/App.jsx
+++ map-create/src/App.jsx
@@ -1,61 +1,75 @@
 import "./style.css"
 
 export default function App() {
-  return <form className="home-form">
-    <div className="header">
-      <div className="headcontent">
-        <button>Navigation</button>
+  return <form className="min-h-screen flex flex-col">
+    <div className="w-screen h-[120px] flex items justify-center items-center bg-green-600">
+      <div className="flex gap-8">
+        <button className="h-[60px] w-[100px] font-semibold rounded-2xl bg-amber-100">Navigation</button>
         <input
         type="text"
-        placeholder="Search..."></input>
+        placeholder="Search..."
+        className="w-[880px] h-[60px] pl-2 border-0 rounded-2xl bg-white"></input>
       </div>
     </div>
 
-    <div className="body">
-      <div className="line">
+    <div className="flex-1 bg-white">
+      <div className="flex gap-15 flex items justify-center items-center">
+        
+        <button className="w-[120px] h-[50px] mt-4 font-medium text-white
+           rounded-2xl  bg-blue-500">All</button>
+        <button className="w-[120px] h-[50px] mt-4 font-medium text-white
+         rounded-2xl bg-blue-500">Luzon</button>
+        <button className="w-[120px] h-[50px] mt-4 font-medium text-white
+         rounded-2xl bg-blue-500">Visayas</button>
+        <button className="w-[120px] h-[50px] mt-4 font-medium text-white
+         rounded-2xl bg-blue-500">Mindanao</button>
 
-        <div className="city">
+      </div>
+
+      <div className="flex gap-10 flex items justify-center items-center mt-8">
+
+        <div className="w-[400px] h-[200px] bg-gray-100 rounded-2xl">
         <div className="body"></div>
-        <div className="description">
+        <div className="bg-black">
           <h2>Davao City</h2>
         </div>
       </div>
 
-      <div className="city">
+      <div className="w-[400px] h-[200px] bg-gray-100 rounded-2xl">
         <div className="body"></div>
-        <div className="description">
+        <div className="bg-black">
           <h2>Davao City</h2>
         </div>
       </div>
 
-      <div className="city">
+      <div className="w-[400px] h-[200px] bg-gray-100 rounded-2xl">
         <div className="body"></div>
-        <div className="description">
+        <div className="bg-black">
           <h2>Davao City</h2>
         </div>
       </div>
 
       </div>
 
-            <div className="line">
+      <div className="flex gap-10 flex items justify-center items-center mt-8">
 
-        <div className="city">
+        <div className="w-[400px] h-[200px] bg-gray-100 rounded-2xl">
         <div className="body"></div>
-        <div className="description">
+        <div className="bg-black">
           <h2>Davao City</h2>
         </div>
       </div>
 
-      <div className="city">
+      <div className="w-[400px] h-[200px] bg-gray-100 rounded-2xl">
         <div className="body"></div>
-        <div className="description">
+        <div className="bg-black">
           <h2>Davao City</h2>
         </div>
       </div>
 
-      <div className="city">
+      <div className="w-[400px] h-[200px] bg-gray-100 rounded-2xl">
         <div className="body"></div>
-        <div className="description">
+        <div className="bg-black ">
           <h2>Davao City</h2>
         </div>
       </div>
